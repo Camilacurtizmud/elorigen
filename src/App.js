6c89e5f7196e8575a012ccfc34c5ace2368a4352
Camilacurtizmud <83440613+Camilacurtizmud@users.noreply.github.com>
2021-11-01 22:00:07 +0100

    Version1/11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from'./components/navbar';
+import NavBar from './components/navbar/navbar';
+import List from './components/list/list';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <List/>
     </div>
   );
 }
